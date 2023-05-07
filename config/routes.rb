959f81eefda3d75cfa848e21240228b0eb9c92bd
StanTim <1694569@mail.ru>
2023-05-07 21:06:21 +0300

    fix: set root path to user#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    root to: "users#show"
+  end
+
 end
