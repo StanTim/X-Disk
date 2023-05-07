91b72830012fd3493d9a4dbc8a282b2ac6b46599
StanTim <1694569@mail.ru>
2023-05-07 14:31:53 +0300

    feat: create user model

--- db/migrate/20230507083151_create_users.rb
+++ db/migrate/20230507083151_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :email, null: false, default: ""
+      t.string :user_name, null: false, default: ""
 
       t.timestamps
     end
