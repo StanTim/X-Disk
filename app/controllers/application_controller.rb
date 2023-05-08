19470fd6cf3f28e8dd384847bfb220f5e1f4f44a
StanTim <1694569@mail.ru>
2023-05-08 14:15:27 +0300

    fix: configure_permitted_parameters for sign in, sign up, update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,29 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name])
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:user_name,
+               :email,
+               :password,
+               :password_confirmation,
+               :remember_me)
+    end
+
+    devise_parameter_sanitizer.permit(:sign_in) do |u|
+      u.permit(:user_name,
+               :password,
+               :remember_me)
+    end
+
+    devise_parameter_sanitizer.permit(:account_update) do |u|
+      u.permit(:user_name,
+               :email,
+               :password,
+               :password_confirmation,
+               :current_password)
+    end
   end
 end
