9f55c4b6d784fbc904033841bf44f79fb9f1b533
StanTim <1694569@mail.ru>
2023-05-08 14:13:54 +0300

    feat: set valid format for pass & user_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true, length: { maximum: 255 }
+
   validates :user_name,
             uniqueness: true,
             length: { maximum: 15 },
             presence: true
+
+  validates_format_of :user_name, with: /\A[a-z0-9]*\z/,
+                      message: 'User name must be Alphanumeric',
+                      multiline: true
+
+  validates_format_of :password,
+                      with: /^(?=.*[A-z])(?=.*[0-9])(?=.*[!@#$%!?`~^&*()\-__+.]).{8,30}$/,
+                      multiline: true,
+                      message: 'Password should have more than 8 and smolder 30 characters,
+                                including uppercase letters,
+                                numbers and special character'
+
 end
