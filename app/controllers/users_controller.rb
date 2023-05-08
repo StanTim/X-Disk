31415f72d009c83868b3fa54644e6e4ed41d7e08
StanTim <1694569@mail.ru>
2023-05-08 14:16:47 +0300

    feat: set up devise path's for actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,40 +2,29 @@ class UsersController < ApplicationController
   before_action :set_current_user, except: [:show]
   before_action :authenticate_user!
 
-  # GET /users/1
-  def show
-  end
-
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
-  end
 
-  # POST /users
   def create
     @user = User.new
 
     if @user.save
       redirect_to @user, notice: 'User was successfully created.'
     else
-      render :new
+      render 'devise/registration/new'
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       redirect_to @user, notice: 'User was successfully updated.'
     else
-      render :edit
+      render 'devise/registration/edit'
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
     redirect_to root_path, notice: 'User was successfully destroyed.'
