6e209204b3956330f34b92c5ed375eb52c40f4d1
StanTim <1694569@mail.ru>
2023-06-29 19:39:11 +0300

    feat: add image_by_file_type method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def image_by_file_type(file_type)
+    images =
+      {
+        Disk::PERMITTED_EXTENTIONS[0] => "zip.svg",
+        Disk::PERMITTED_EXTENTIONS[1] => "xls.svg",
+        Disk::PERMITTED_EXTENTIONS[2] => "doc.svg"
+      }
+
+    image_tag asset_pack_path("media/images/#{images[file_type]}")
+  end
 end
